c500299bf816733cad36d1262719cf9d37c17575
Nycholas de Oliveira e Oliveira <nycholas@gmail.com>
2022-10-20 16:34:59 -0300

    Fix types from previous release and update to new release

--- stubs/Flask-JSONRPC/flask_jsonrpc/__init__.pyi
+++ stubs/Flask-JSONRPC/flask_jsonrpc/__init__.pyi
@@ -1,5 +1,3 @@
 from .app import JSONRPC as JSONRPC
 from .blueprints import JSONRPCBlueprint as JSONRPCBlueprint
 from .views import JSONRPCView as JSONRPCView
-
-__all__ = ["JSONRPC", "JSONRPCBlueprint", "JSONRPCView"]
